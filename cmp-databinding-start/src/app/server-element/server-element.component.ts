221b2c3b86a25a6ec8ef0280e07ec895d809ac21
Adan <twinadanz@gmail.com>
2018-09-20 22:53:39 -0500

    add styles and ViewEncapsulation property

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+  // View Encapsulation is how Angular applies styles to only specific components and not have them 'bleed' into others.
+  // .Emulated is the default behavior, which is emulating the shadow DOM, a copy of the DOM to make changes to the real DOM.
 })
 export class ServerElementComponent implements OnInit {
    // This uses an input alias. The template uses srvElement, but actually binds to 'element' property.
