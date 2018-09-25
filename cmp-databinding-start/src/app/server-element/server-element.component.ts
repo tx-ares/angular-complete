cc21a6e3e1f191c20b1412bc066130ea00223026
Adan <twinadanz@gmail.com>
2018-09-24 23:11:57 -0500

    test life cycle hooks

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges,
+         DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,13 +9,44 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   // View Encapsulation is how Angular applies styles to only specific components and not have them 'bleed' into others.
   // .Emulated is the default behavior, which is emulating the shadow DOM, a copy of the DOM to make changes to the real DOM.
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, OnDestroy,
+             DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit {
    // This uses an input alias. The template uses srvElement, but actually binds to 'element' property.
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log('contructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+   console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewChecked() {
+   console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewInit() {
+   console.log('ngAfterViewInit called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
   }
 
 }
