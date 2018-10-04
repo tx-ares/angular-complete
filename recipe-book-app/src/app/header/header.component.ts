6286c232948953b017e02f7a28f58ba1834fccab
Adan <twinadanz@gmail.com>
2018-10-03 23:25:14 -0500

    add event emitter

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+    @Output() viewChanged = new EventEmitter<{currentView: string}>();
+
+    public changeView(view: string) {
+        console.log('changeView fired!');
+        this.viewChanged.emit({currentView: view});
+    }
+}
