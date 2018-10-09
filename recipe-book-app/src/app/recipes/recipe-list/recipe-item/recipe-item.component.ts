b3add80cc5ad1b079350af71a2025092c08ff0af
Adan Huerta <Twinadanz@gmail.com>
2018-10-08 23:29:40 -0500

    improve solution

--- recipe-book-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-book-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input() recipes: any[];
+  @Input() recipe: Recipe;
 
   constructor() { }
 
