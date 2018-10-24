e12beba614f22a5d8298d0d5f6c47b0974b4b05b
Adan Huerta <Twinadanz@gmail.com>
2018-10-23 23:42:11 -0500

    use hostbinding for style change

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,16 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive,
+         Renderer2,
+         OnInit,
+         ElementRef,
+         HostListener,
+         HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // With host binding, we gain access to the elements attributes inside the component.
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private renderer: Renderer2,
               private elRef: ElementRef) { }
@@ -13,11 +20,13 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'goldenrod');
+    // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'goldenrod');
+    this.backgroundColor = 'goldenrod';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
 }
