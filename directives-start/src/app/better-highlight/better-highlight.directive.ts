579d51384474ea088fda5f9e00bf2a4ce893905c
Adan Huerta <Twinadanz@gmail.com>
2018-10-24 23:35:43 -0500

    add property binded values

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -3,12 +3,15 @@ import { Directive,
          OnInit,
          ElementRef,
          HostListener,
-         HostBinding } from '@angular/core';
+         HostBinding, 
+         Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'dodgerblue';
   // With host binding, we gain access to the elements attributes inside the component.
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
@@ -16,17 +19,18 @@ export class BetterHighlightDirective implements OnInit {
               private elRef: ElementRef) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'goldenrod');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'goldenrod');
-    this.backgroundColor = 'goldenrod';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
