1644dc8521d1471ded1838db54516cf4cdcb9b70
Adan Huerta <Twinadanz@gmail.com>
2018-10-29 22:59:20 -0500

    add custom dropdown directive

--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { IngredientComponent } from './shopping-list/models/ingredient/ingredient.component';
 import { HeaderComponent } from './header/header.component';
+import { DropDownDirective } from './shared/drop-down.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeaderComponent } from './header/header.component';
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListEditComponent,
-    IngredientComponent
+    IngredientComponent,
+    DropDownDirective
   ],
   imports: [
     BrowserModule,
