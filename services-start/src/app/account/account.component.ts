52c83dca84bbc479e485b44a5099faf4074c8e6b
Adan Huerta <Twinadanz@gmail.com>
2018-10-30 22:17:48 -0500

    add update service method to account component

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -16,6 +16,7 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
+    this.accountsService.statusUpdated.emit(status);
     // this.loggingService.logStatusChange(status);
   }
 }
