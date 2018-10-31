cdcbb910d81a68b015bf91651439e281be02eea2
Adan Huerta <Twinadanz@gmail.com>
2018-10-30 22:16:16 -0500

    move services to module level, refactor event emitters

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../services/logging.service';
 import { AccountsService } from '../services/accounts.service';
 
@@ -6,14 +6,20 @@ import { AccountsService } from '../services/accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService] // By leaving out the AccountsService we can make sure that we don't create any more instances of the service.  This is important for data consistency across our components.
-})
+  // providers: [LoggingService]
+}) // By leaving out the AccountsService we can make sure that we don't create any more instances of the service.  This is important for data consistency across our components.
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { }
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- services-start/src/app/services/accounts.service.ts
+++ services-start/src/app/services/accounts.service.ts
@@ -1,4 +1,8 @@
-export class AccountsService {
+import { LoggingService } from './logging.service';
+import { Injectable, EventEmitter } from '@angular/core';
+
+@Injectable() // Injectable decorator can be used to add metadata like whats passed into @Component() or @Directive().  Angular needs this in order to inject a service within another service.
+export class AccountsService { //  Important to note, that the @Injectable decorator belongs on the class/service on which you are injecting another service into.
     accounts = [
         {
           name: 'Master Account',
@@ -13,6 +17,10 @@ export class AccountsService {
           status: 'unknown'
         }
       ];
+
+    statusUpdated = new EventEmitter<string>();
+
+    constructor(private loggingService: LoggingService) { }
 
     addAccount(name: string, status: string) {
         this.accounts.push({name: name, status: status});
