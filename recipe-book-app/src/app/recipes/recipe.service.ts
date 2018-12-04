6a8aa0e87ae4728a6735bdd2bd2ba48ab03bf275
Adan Huerta <Twinadanz@gmail.com>
2018-12-03 19:41:51 -0600

    add ingredient data to recipes

--- recipe-book-app/src/app/recipes/recipe.service.ts
+++ recipe-book-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './models/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
@@ -9,11 +10,22 @@ export class RecipeService {
     new Recipe(
       'Cheesy Steaks',
       'They are very cheesy',
-      'https://assets.epicurious.com/photos/57d0394f7d2e71cf344f18a8/master/pass/philly-cheese-steak.jpg'),
+      'https://assets.epicurious.com/photos/57d0394f7d2e71cf344f18a8/master/pass/philly-cheese-steak.jpg',
+      [
+        new Ingredient('Chopped Steak', 1),
+        new Ingredient('Pepper Jack Cheese', 3)
+      ]),
     new Recipe(
       'Meatza',
       'It is super meaty',
-      'https://truffle-assets.imgix.net/7acd2776-1507_meatza_land1.jpg')
+      'https://truffle-assets.imgix.net/7acd2776-1507_meatza_land1.jpg',
+      [
+        new Ingredient('Pizza Dough', 1),
+        new Ingredient('Tomato Sauce', 1),
+        new Ingredient('Sausage', 2),
+        new Ingredient('Ground Beef', 2),
+        new Ingredient('Venison', 2),
+      ])
   ];
 
   getRecipes() {
