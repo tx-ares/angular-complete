d37ffc37b7ded0bbec6aa655caa9973892e56ae4
Adan Huerta <Twinadanz@gmail.com>
2018-12-03 20:29:59 -0600

    fix style file name

--- prj-services-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-services-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,7 @@ import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
