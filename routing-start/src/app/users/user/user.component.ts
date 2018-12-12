516772fa5f662de1ff3814d8ea91c53d421a7bd8
Adan Huerta <Twinadanz@gmail.com>
2018-12-11 22:29:19 -0600

    add cleanup for subscriptions onDestroy

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -23,6 +25,12 @@ export class UserComponent implements OnInit {
           this.user.name = params['name'];
         }
       );
+  }
+
+  // For route param changes, unsubscribing is handled automatically by angular, but for custom Observables,
+  // It is important to unsubscribe from Observables once the component is destroyed.
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
