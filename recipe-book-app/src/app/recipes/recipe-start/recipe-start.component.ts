e775ae2c4590ded8792028ce7227802c08bce9d5
Adan Huerta <Twinadanz@gmail.com>
2019-01-06 20:40:35 -0600

    add recipe start component

---
+++ recipe-book-app/src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,10 @@
+import { OnInit, Component } from "@angular/core";
+
+@Component({
+  selector: 'recipe-start',
+  templateUrl: './recipe-start.component.html',
+  styleUrls: ['./recipe-start.component.css'],
+})
+export class RecipeStartComponent implements OnInit {
+  ngOnInit() { }
+ }
