6c1451945093216aa16b9b508475a7165fd9aacc
Adan Huerta <Twinadanz@gmail.com>
2019-01-06 19:36:06 -0600

    add proper routerLinks

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    @Output() viewChanged = new EventEmitter<{currentView: string}>();
 
-    public changeView(view: string) {
-        console.log('changeView fired!');
-        this.viewChanged.emit({currentView: view});
-    }
 }
