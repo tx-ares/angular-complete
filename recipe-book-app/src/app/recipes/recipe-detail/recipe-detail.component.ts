f92c8166115d20bcafcffc3b0275296adb851316
Adan Huerta <Twinadanz@gmail.com>
2019-02-04 22:21:26 -0600

    add router , fix  property error

--- recipe-book-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -17,6 +17,7 @@ export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
   constructor(private shoppingListService: ShoppingListService, 
+              private router: Router,
               private route: ActivatedRoute,
               private recipeService: RecipeService) { }
 
@@ -34,6 +35,10 @@ export class RecipeDetailComponent implements OnInit {
     selectedRecipe.ingredients.forEach((ingredient) => {
       this.shoppingListService.addIngredient(ingredient);
     });
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route})
   }
 
 
