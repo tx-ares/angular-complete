9a35e335635cd9d7a900ef0b7f928b6358bc86c9
Adan Huerta <Twinadanz@gmail.com>
2019-02-04 22:31:13 -0600

    clean up

--- recipe-book-app/src/app/app.component.ts
+++ recipe-book-app/src/app/app.component.ts
@@ -9,8 +9,5 @@ export class AppComponent {
   public title = 'app';
   public currentView = 'recipes';
 
-  public onViewChanged(viewName: {currentView: string}) {
-    console.log('onViewChanged fired!');
-    this.currentView = viewName.currentView;
-  }
+
 }
