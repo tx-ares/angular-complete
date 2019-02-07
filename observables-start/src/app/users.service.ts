97d5ccf32082efc68c597dab6df8da4a21fec20d
Adan Huerta <Twinadanz@gmail.com>
2019-02-06 23:36:24 -0600

    add users service

---
+++ observables-start/src/app/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  userActivated = new Subject();
+
+  constructor() { }
+}
