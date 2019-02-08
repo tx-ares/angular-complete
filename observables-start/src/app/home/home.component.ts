460c73435ecebf0b56bc798ff27924d8e4e24b5b
Adan Huerta <Twinadanz@gmail.com>
2019-02-07 23:16:35 -0600

    fix updates, fix pipe for operators

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
 
-import 'rxjs/Rx';
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Rx';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -18,11 +17,11 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     const myNumbers = Observable.interval(1000)
-      .map( // the map operator from rxjs will take the data emitted and can be used to manipulate the data.
+      .pipe(map( // the map operator from rxjs will take the data emitted and can be used to manipulate the data.
         (data: number) => {
           return data * 2; // here it will simply double the emitted value.
         }
-      );
+      ));
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: Number) => {
         console.log(number);
