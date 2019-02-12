4d33b0538db262e297ecb8faecdb65faf838c204
Adan Huerta <Twinadanz@gmail.com>
2019-02-11 23:31:16 -0600

    clean up service , use subject

--- prj-services-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-services-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
