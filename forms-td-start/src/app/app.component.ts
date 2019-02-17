3a2cca2884b8fbd570b84ba226c7cb8e51293e6e
Adan Huerta <Twinadanz@gmail.com>
2019-02-16 23:46:00 -0600

    add defaultQuestion property

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('formEL') signupForm: NgForm;
+  defaultQuestion = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
