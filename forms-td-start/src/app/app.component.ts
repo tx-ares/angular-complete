3f712fb5980868fbb861d4937444ebcafaba3d36
Adan Huerta <Twinadanz@gmail.com>
2019-02-24 19:56:33 -0600

    add form reset

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -37,5 +37,7 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
     this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
