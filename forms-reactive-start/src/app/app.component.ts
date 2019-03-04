2db64fbfec2a93baee5e98b4f6afe07ed29de869
Adan Huerta <Twinadanz@gmail.com>
2019-03-03 20:25:38 -0600

    fix lint

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.signupForm, ' << signupForm')
+    console.log(this.signupForm, ' << signupForm');
   }
 
 }
