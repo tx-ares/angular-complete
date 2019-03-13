4b40ffd2b06aa0bd079ec3836de06370155c33a4
Adan Huerta <Twinadanz@gmail.com>
2019-03-12 23:19:04 -0500

    add signatures to methods

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   private signupForm: FormGroup;
   forbiddenUserNames = ['Jim', 'Lisa'];
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [ Validators.required, this.forbiddenNames.bind(this)]),
@@ -23,23 +23,23 @@ export class AppComponent implements OnInit {
     });
   }
 
-  onAddHobby() {
+  public onAddHobby(): void {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
-  onSubmit() {
+  public onSubmit(): void {
     console.log(this.signupForm, ' << signupForm');
   }
 
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  public forbiddenNames(control: FormControl): {[s: string]: boolean} {
     if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
     }
     return null;
   }
 
-  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+  public forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
     const promise = new Promise<any>((resolve, reject) => {
       setTimeout(() => {
         if (control.value === 'test@test.com') {
