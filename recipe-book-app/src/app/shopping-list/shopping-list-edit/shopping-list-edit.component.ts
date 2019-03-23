07ce70cf0ca74df2b3ce493bc214b97fac2ab03b
Adan Huerta <Twinadanz@gmail.com>
2019-03-22 22:30:38 -0500

    refactor

--- recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,26 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
-
-  @ViewChild('nameInput') nameInput: ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef;
+export class ShoppingListEditComponent {
 
   constructor(private shoppingListService: ShoppingListService) { }
 
-  ngOnInit() {
-  }
-
-  onAddItem() {
-    const ingredientName = this.nameInput.nativeElement.value;
-    const ingredientAmount = this.amountInput.nativeElement.value;
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+  public onAddItem(form: NgForm): void {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
 
