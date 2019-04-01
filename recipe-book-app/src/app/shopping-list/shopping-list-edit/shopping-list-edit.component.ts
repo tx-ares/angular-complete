5cc238eac13fadd6f6a1f4c48a49e90ea0168030
Adan Huerta <Twinadanz@gmail.com>
2019-03-31 23:30:42 -0500

    add subscription

--- recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -9,6 +10,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
+  private subscription: Subscription;
+  public editMode: boolean = false;
+  public editedItemIndex: number;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
@@ -16,6 +20,18 @@ export class ShoppingListEditComponent {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
+  }
+
+  public ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
