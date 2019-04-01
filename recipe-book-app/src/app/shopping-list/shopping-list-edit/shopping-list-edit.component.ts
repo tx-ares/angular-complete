cb047622ddf9fd9abd2b77dfc2ed6deef36d38ee
Adan Huerta <Twinadanz@gmail.com>
2019-03-31 23:40:17 -0500

    add viewchild and update form dynamically

--- recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
@@ -10,9 +10,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
+  @ViewChild('f') shoppingListForm: NgForm;
   private subscription: Subscription;
   public editMode: boolean = false;
   public editedItemIndex: number;
+  public editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
@@ -27,6 +29,11 @@ export class ShoppingListEditComponent {
       .subscribe((index: number) => {
         this.editMode = true;
         this.editedItemIndex = index;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shoppingListForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
       });
   }
 
