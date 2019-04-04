fa129f15522ce0b279c0bc181139cc4759c8899d
Adan Huerta <Twinadanz@gmail.com>
2019-04-03 23:26:29 -0500

    clean up

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent { }
