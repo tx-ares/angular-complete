417959ec8ac44c2ab1ce9a1bc3ecdd89ed947bf0
Adan Huerta <Twinadanz@gmail.com>
2019-04-03 23:27:27 -0500

    clean up

--- recipe-book-app/src/app/shopping-list/models/ingredient/ingredient.component.ts
+++ recipe-book-app/src/app/shopping-list/models/ingredient/ingredient.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ingredient',
   templateUrl: './ingredient.component.html',
   styleUrls: ['./ingredient.component.css']
 })
-export class IngredientComponent implements OnInit {
-
-  constructor() { }
-
-  public ngOnInit(): void {
-  }
-
-}
+export class IngredientComponent { }
