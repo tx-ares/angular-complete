69c94c8a3bc53d73fad2caf6a53c28a6426311c9
Adan Huerta <Twinadanz@gmail.com>
2019-04-14 09:45:59 -0500

    add reactiveFormsModule

--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -37,6 +37,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRouterModule
   ],
   providers: [ShoppingListService],
