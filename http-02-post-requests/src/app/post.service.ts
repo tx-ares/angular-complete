63eca2ad583d118804093c9606f46860c7f2d574
Adan Huerta <Twinadanz@gmail.com>
2019-09-01 16:28:19 -0500

    create error subject

--- http-02-post-requests/src/app/post.service.ts
+++ http-02-post-requests/src/app/post.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  public error = new Subject<string>();
 
   constructor(private http: HttpClient) {}
 
