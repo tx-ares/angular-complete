55dce1f0a12933e2c70c4c6503d08e7905a398f4
Adan Huerta <Twinadanz@gmail.com>
2019-09-01 16:01:13 -0500

    refactor service to return observables

--- http-02-post-requests/src/app/post.service.ts
+++ http-02-post-requests/src/app/post.service.ts
@@ -13,31 +13,25 @@ export class PostService {
   public createAndStorePost(title: string, content: string) {
     const postData: Post = { title, content };
 
-    this.http
-    .post<{ name: string }>( // You can also type cast http client's post & get methods to inform TypeScript what type of object is being retrieved or sent.
-      'https://angular-complete-b4f4a.firebaseio.com/posts.json', // Firebase's api requires that we send a request as JSON in the url
-      postData
-    )
-    .subscribe(responseData => {
-      console.log(responseData);
-    });
+    return this.http
+            .post<{ name: string }>( // You can also type cast http client's post method to inform TypeScript what type of object is being retrieved or sent.
+              'https://angular-complete-b4f4a.firebaseio.com/posts.json', // Firebase's api requires that we send a request as JSON in the url
+              postData
+            );
   }
 
   public fetchPosts() {
-    this.http.get('https://angular-complete-b4f4a.firebaseio.com/posts.json')
-    .pipe(
-      map((responseData: {[key: string]: Post }) => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({...responseData[key], id: key });
-          }
-        }
-        return postsArray;
-      })
-    )
-    .subscribe( (posts) => {
-      //
-    });
+    return this.http.get('https://angular-complete-b4f4a.firebaseio.com/posts.json')
+            .pipe(
+              map((responseData: {[key: string]: Post }) => {
+                const postsArray: Post[] = [];
+                for (const key in responseData) {
+                  if (responseData.hasOwnProperty(key)) {
+                    postsArray.push({...responseData[key], id: key });
+                  }
+                }
+                return postsArray;
+              })
+            );
   }
 }
