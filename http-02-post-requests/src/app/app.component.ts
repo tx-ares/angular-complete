38d4f1dc5124c1c6d0bec339784ac75885f6c5d1
Adan Huerta <Twinadanz@gmail.com>
2019-09-01 16:28:37 -0500

    add onDestroy for unsubbing observables

--- http-02-post-requests/src/app/app.component.ts
+++ http-02-post-requests/src/app/app.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   public loadedPosts: Post[] = [];
   public isFetching = false;
   public error = null;
+  public subscriptions: Subscription[] = [];
 
   constructor(private http: HttpClient,
               private postService: PostService) {}
 
   public ngOnInit() {
     this.isFetching = true;
-    this.postService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
+
+    this.subscriptions.push(this.postService.error.subscribe((errorMessage) => {
+      this.error.next(errorMessage);
+    }));
+
+    this.subscriptions.push(this.postService.fetchPosts().subscribe(
+      posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
     }, error => {
       this.error = error.message;
-      console.log(error);
-    });
+    }));
   }
 
   public onCreatePost(postData: Post) {
@@ -34,15 +41,19 @@ export class AppComponent implements OnInit {
 
   public onFetchPosts() {
     this.isFetching = true;
-    this.postService.fetchPosts().subscribe(posts => {
+    this.subscriptions.push(this.postService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    });
+    }));
   }
 
   public onClearPosts() {
-    this.postService.clearAllPosts().subscribe(() => {
+    this.subscriptions.push(this.postService.clearAllPosts().subscribe(() => {
       this.loadedPosts = [];
-    });
+    }));
+  }
+
+  public ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
