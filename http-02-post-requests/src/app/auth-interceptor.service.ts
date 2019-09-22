1916151e2f8a8b2d8d888750ec2d3bc7a2a3ab43
Adan Huerta <Twinadanz@gmail.com>
2019-09-22 15:12:19 -0500

    clean up auth interceptor

--- http-02-post-requests/src/app/auth-interceptor.service.ts
+++ http-02-post-requests/src/app/auth-interceptor.service.ts
@@ -3,17 +3,10 @@ import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   public intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('request is on its way');
 
     const modifiedRequest = req.clone({
         headers: req.headers.append('Auth', '123')
       });
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      console.log(event);
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived, body data: ');
-        console.log(event.body);
-      }
-    }));
+    return next.handle(modifiedRequest);
   }
 }
