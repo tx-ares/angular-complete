4d437f452af6ac695bc22c7ed9d4d9fd9f74e264
Adan Huerta <v802588@global.ual.com>
2019-09-25 15:49:16 -0500

    add resolver to routes

--- recipe-book-app/src/app/app-router.module.ts
+++ recipe-book-app/src/app/app-router.module.ts
@@ -5,15 +5,16 @@ import { RecipeBookComponent } from './recipes/recipes.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // This sets the default path matching strategy to full, meaning it will only redirect if the FULL path is '' or empty.
-  { path: 'recipes', component: RecipeBookComponent, 
+  { path: 'recipes', component: RecipeBookComponent,
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent } 
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
     ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: '**', redirectTo: '/not-found' } // catch invalid routes. MUST be last route because routes are parsed in order they're defined.
