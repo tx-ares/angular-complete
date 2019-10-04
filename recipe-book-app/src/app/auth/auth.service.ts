1c1c050f95dfa09712c6b6aca7104582c886863b
Adan Huerta <v802588@global.ual.com>
2019-10-04 16:00:24 -0500

    add login method

--- recipe-book-app/src/app/auth/auth.service.ts
+++ recipe-book-app/src/app/auth/auth.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered? : boolean;
 };
 
 @Injectable({
@@ -20,7 +22,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   public signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDDZfV0FThF4uvRI-fhJsy4AAQ8vSuEpII',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKEY,
       {
         email: email,
         password: password,
@@ -38,5 +40,16 @@ export class AuthService {
 
       return throwError(errorMessage);
     }));
+  }
+
+
+  public login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' + environment.firebaseAPIKEY,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
   }
 }
