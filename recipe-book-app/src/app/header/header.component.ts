6f0c684476b57fdbf902cf5a33b2cb433c2dbc2b
Adan Huerta <v802588@global.ual.com>
2019-10-09 15:54:08 -0500

    add user subscription,  implement auth check on init

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from 'app/shared/data-storage.service';
+import { AuthService } from 'app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  private isAuthenticated = false;
 
-  public onSaveData() {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) { }
+
+  public ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      // this.isAuthenticated = !user ? false : true;
+      this.isAuthenticated = !!user; // This shorthand trick means that if user exists, return true, if it doesnt, return false.
+      // console.log(!user);
+      // console.log(!!user);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
+  public onSaveData(): void {
     this.dataStorageService.storeRecipes();
   }
 
-  public onFetchData() {
+  public onFetchData(): void {
     this.dataStorageService.fetchRecipes().subscribe();
   }
 }
