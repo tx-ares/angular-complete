8e8434d10bc314b4da77703edec408ed35afa708
Adan Huerta <v802588@global.ual.com>
2019-10-09 14:26:13 -0500

    create user model

---
+++ recipe-book-app/src/app/auth/user/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) { }
+
+  get token() { // Notice that in the above constructor token and tokenExpirationDate are private.  This is because we don't want these attributes to be able to be publically accessed and will be set programmatically by using a 'getter' function.
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate)
+    return this._token;
+  }
+}
