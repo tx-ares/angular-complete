8c1ba1bd6c598e9f4fb6da8a8239be4207ca2dd3
Adan Huerta <v802588@global.ual.com>
2019-12-04 13:48:53 -0600

    add message & close props

--- recipe-book-app/src/app/shared/alert/alert.component.ts
+++ recipe-book-app/src/app/shared/alert/alert.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent {
+  @Input() message: string;
+  @Output() close = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit() {
+  public onClose() {
+    this.close.emit();
   }
 
 }
