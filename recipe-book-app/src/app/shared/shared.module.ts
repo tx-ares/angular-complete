955cebc5e675fd9986c778a74392a9b3f04e2a19
Adan Huerta <v802588@global.ual.com>
2019-12-04 15:39:45 -0600

    create shared module

---
+++ recipe-book-app/src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertComponent } from './alert/alert.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropDownDirective } from './drop-down.directive';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropDownDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropDownDirective,
+    CommonModule
+  ]
+})
+export class SharedModule { }
