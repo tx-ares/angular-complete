64306969cdfab2e685cae7b4273b8d76f865e7ce
Adan Huerta <v802588@global.ual.com>
2019-12-04 15:40:16 -0600

    add sharedModule to inports

--- recipe-book-app/src/app/recipes/recipes.module.ts
+++ recipe-book-app/src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RecipesRouterModule } from './recipes-routing.module';
+import { SharedModule } from 'app/shared/shared.module';
 
 
 @NgModule({
@@ -19,10 +20,10 @@ import { RecipesRouterModule } from './recipes-routing.module';
     RecipeDetailComponent
   ],
   imports: [ // Remember that in feature modules, directive modules like these must be included in the feature module, and not the AppModule.  Services are an exception since these can be instantiated once as a singleton service and be available to the entire application.
-    CommonModule, // Template directives like ngIf and ngFor are usually provided by BrowserModule, however, BrowserModule can only be imported once in an Angular application since it handles start up processes as well as providing the template directives.  For that reason, you can use CommonModule to gain access to the template directives ONLY.
     ReactiveFormsModule,
     RouterModule,
-    RecipesRouterModule
+    RecipesRouterModule,
+    SharedModule
   ],
   // Since the components are only being used internally in the recipes module, these components don't need to be exported anymore as they won't be loaded anywhere else.
   // exports: [
