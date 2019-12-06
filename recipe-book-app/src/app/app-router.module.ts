c192add95415161b9604d94558ed096b54b57f86
Adan Huerta <twinadanz@gmail.com>
2019-12-06 13:34:32 -0600

    add lazy load routes for auth and shopping-list

--- recipe-book-app/src/app/app-router.module.ts
+++ recipe-book-app/src/app/app-router.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
  const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // This sets the default path matching strategy to full, meaning it will only redirect if the FULL path is '' or empty.
-  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }// This special syntax is used for lazy loading a component.  By using this filepath + # + Module name, we can tell Angular what to load at what route.
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) }
 ];
 
 @NgModule({ // This decorator changes this typescript class into an Angular Module.
