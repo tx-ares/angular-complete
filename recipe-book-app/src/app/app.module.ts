5ded55d559fc09c6552c956ed4ed505bcb27f35e
Adan Huerta <twinadanz@gmail.com>
2019-12-06 12:21:51 -0600

    clean up declartions and imports

--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { IngredientComponent } from './shopping-list/models/ingredient/ingredient.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRouterModule } from './app-router.module';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AlertComponent } from './shared/alert/alert.component';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -20,16 +17,13 @@ import { AuthModule } from './auth/auth.module';
   declarations: [ // Important thing to know is that Components, Pipes, Directives, etc. can only be declared ONCE in the ENTIRE APPLICATION. Make sure this is the case when using multiple feature modules.
     AppComponent,
     HeaderComponent,
-    PageNotFoundComponent,
-    IngredientComponent,
-    RecipeEditComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRouterModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
