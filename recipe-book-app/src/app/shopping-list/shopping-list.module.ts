51d995cda132401baa6ef1600324747063b6bdf9
Adan Huerta <twinadanz@gmail.com>
2019-12-06 14:06:15 -0600

    add missing ingredient component

--- recipe-book-app/src/app/shopping-list/shopping-list.module.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.module.ts
@@ -5,11 +5,13 @@ import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-ed
 import { ShoppingListRouterModule } from './shopping-list-router.module';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { IngredientComponent } from './models/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
     ShoppingListComponent,
     ShoppingListEditComponent,
+    IngredientComponent
   ],
   imports: [
     CommonModule, // Template directives like ngIf and ngFor are usually provided by BrowserModule, however, BrowserModule can only be imported once in an Angular application since it handles start up processes as well as providing the template directives.  For that reason, you can use CommonModule to gain access to the template directives ONLY.
