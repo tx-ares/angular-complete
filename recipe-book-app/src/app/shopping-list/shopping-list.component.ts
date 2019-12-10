eddf8af46f247d79eda229651643490a04f8f761
Adan Huerta <twinadanz@gmail.com>
2019-12-10 10:59:21 -0600

    replace rxjs subscribe usage for ngrx store

--- recipe-book-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.component.ts
@@ -3,31 +3,28 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from 'app/logging.service';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[]}>;
 
   constructor(
     private shoppingListService: ShoppingListService,
-    private loggingService: LoggingService) { }
+    private loggingService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) // Store must be defined as reducer map object, which contains the reducer as a key and its return value as a value.
+  { }
 
   public onEditItem(index: number): void {
     this.shoppingListService.startedEditing.next(index);
   }
 
   public ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => {
-          this.ingredients = ingredients;
-        }
-      );
+    this.ingredients = this.store.select('shoppingList');
     this.loggingService.printLog('Hi from ShoppingListComponent ngOnInit!');
   }
 
