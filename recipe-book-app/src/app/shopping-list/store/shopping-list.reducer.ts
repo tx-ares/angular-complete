4e77dbf3470fc2d607c579e4b7361b79638a79fd
Adan Huerta <twinadanz@gmail.com>
2019-12-10 10:02:01 -0600

    move shopping-list reducer to store folder

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from "app/shared/ingredient.model";
 import { Action } from "@ngrx/store";
 
+import { ADD_INGREDIENT } from './shopping-list.actions';
+
 const initialState = {
   ingredients: [
     new Ingredient('Apples', 5),
@@ -11,7 +13,7 @@ const initialState = {
 export function shoppingListReducer(state = initialState, action: Action ) { // Reducer will handle state updates by creating a copy of it then making changes to the copy, remember that STORE is IMMUTABLE.
   // note that if state input is not provided, it will default to initialState variable.
   switch(action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action]
