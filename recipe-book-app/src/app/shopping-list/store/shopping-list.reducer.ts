a2d39699d4ec9e0f7301432fe83ce0c305679c43
Adan Huerta <twinadanz@gmail.com>
2019-12-10 10:07:37 -0600

    refactor reducer to use ShoppingListActions class

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from "app/shared/ingredient.model";
 import { Action } from "@ngrx/store";
 
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
@@ -10,13 +10,16 @@ const initialState = {
   ]
 };
 
-export function shoppingListReducer(state = initialState, action: Action ) { // Reducer will handle state updates by creating a copy of it then making changes to the copy, remember that STORE is IMMUTABLE.
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient // Its important to use the correct type so that we have access to the payload ( or any other properties we define in the ShoppingListActions class. )
+ ) { // Reducer will handle state updates by creating a copy of it then making changes to the copy, remember that STORE is IMMUTABLE.
   // note that if state input is not provided, it will default to initialState variable.
   switch(action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
+        ingredients: [...state.ingredients, action.payload]
       }; // Add old state object to this object, effectively making a copy.
   }
 
