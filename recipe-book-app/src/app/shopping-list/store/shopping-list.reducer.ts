942c12f486a29e49973a9ff735cf90b1d5bd4c35
Adan Huerta <twinadanz@gmail.com>
2019-12-11 10:47:36 -0600

    add AddIngredients case to reducer

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,5 +1,4 @@
 import { Ingredient } from "app/shared/ingredient.model";
-import { Action } from "@ngrx/store";
 
 import * as ShoppingListActions from './shopping-list.actions';
 
@@ -11,18 +10,23 @@ const initialState = {
 };
 
 export function shoppingListReducer(
-  state = initialState,
-  action: ShoppingListActions.AddIngredient // Its important to use the correct type so that we have access to the payload ( or any other properties we define in the ShoppingListActions class. )
- ) { // Reducer will handle state updates by creating a copy of it then making changes to the copy, remember that STORE is IMMUTABLE.
-  // note that if state input is not provided, it will default to initialState variable.
-  switch(action.type) {
+  state = initialState,  // note that if state input is not provided, it will default to initialState variable.
+  action: ShoppingListActions.ShoppingListActions // Its important to use the correct type so that we have access to the payload ( or any other properties we define in the ShoppingListActions class. )
+) { // Reducer will handle state updates by creating a copy of it then making changes to the copy, remember that STORE is IMMUTABLE.
+  switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
+      }; // Add old state object to this object, effectively making a copy.
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
       }; // Add old state object to this object, effectively making a copy.
     default:
       return state;
   }
 
 }
+
