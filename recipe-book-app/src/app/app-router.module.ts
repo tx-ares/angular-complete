fc0f29557277f30803f9bf6eb4213a1aab622877
Adan Huerta <twinadanz@gmail.com>
2019-12-11 11:10:51 -0600

    add string vs import methods notation

--- recipe-book-app/src/app/app-router.module.ts
+++ recipe-book-app/src/app/app-router.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
  const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // This sets the default path matching strategy to full, meaning it will only redirect if the FULL path is '' or empty.
+
+  // Routes work with 'string method' of loading children in Angular 8 and below.
+  // { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+  // { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
+  // { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' },
+
+  // Routes work with new 'import method' only Angular 8 and above.
   { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) }
