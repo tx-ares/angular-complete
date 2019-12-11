092662c90f71990159df0a36d117caebcf95e6ec
Adan Huerta <twinadanz@gmail.com>
2019-12-11 10:12:50 -0600

    add payload to constructor

--- recipe-book-app/src/app/shopping-list/store/shopping-list.actions.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,5 +5,6 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // Our new AddIngredient class will inherit properties of Action class. And then we add a readonly property that simply tells TypeScript that this property must never be changed.
-  payload: Ingredient;
+
+  constructor(public payload: Ingredient) { } // By adding payload to the constructor I can now pass in a payload argument to the AddIngredient constructor class.  ex) new ShoppingListActions.AddIngredient(payloadObject)
 }
