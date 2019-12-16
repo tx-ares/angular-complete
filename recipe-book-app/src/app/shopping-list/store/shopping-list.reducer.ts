334b635bae9b9d3afbc26508fcc86a50f81d4cfa
Adan Huerta <twinadanz@gmail.com>
2019-12-16 16:33:54 -0600

    use state data instead of action payloads

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -38,24 +38,28 @@ export function shoppingListReducer(
         ingredients: [...state.ingredients, ...action.payload]
       }; // Add old state object to this object, effectively making a copy.
     case ShoppingListActions.UPDATE_INGREDIENT:
-      const ingredient = state.ingredients[action.payload.index];
+      const ingredient = state.ingredients[state.editedIngredientIndex];
       const updatedIngredient = {
         ...ingredient,
-        ...action.payload.ingredient
+        ...action.payload
       };
       const updatedIngredients = [...state.ingredients];
-      updatedIngredients[action.payload.index] = updatedIngredient;
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
 
       return {
         ...state,
-        ingredients: updatedIngredients
+        ingredients: updatedIngredients,
+        editedIngredientIndex: -1,
+        editedIngredient: null
       };
     case ShoppingListActions.DELETE_INGREDIENT:
       return {
         ...state,
         ingredients: state.ingredients.filter((ingredient, index) => {
-          return index !== action.payload; // payload is the index.
-        })
+          return index !== state.editedIngredientIndex; // payload is the index.
+        }),
+        editedIngredientIndex: -1,
+        editedIngredient: null
       };
     case ShoppingListActions.START_EDIT:
       return {
