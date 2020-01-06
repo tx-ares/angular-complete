dc063f52d6db65838ccec99491efb5627b98a6b4
Adan Huerta <twinadanz@gmail.com>
2020-01-06 15:02:49 -0600

    add action prefixes

--- recipe-book-app/src/app/shopping-list/store/shopping-list.actions.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'app/shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const ADD_INGREDIENT = '[Shopping List] ADD_INGREDIENT';  // It is a good practice to prefix your actions with the feature they're associated with because action dispatches are sent to ALL reducers.  For this reason, we want to make sure the actions as distinct as possible.
+export const ADD_INGREDIENTS = '[Shopping List] ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = '[Shopping List] UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = '[Shopping List] DELETE_INGREDIENT';
+export const START_EDIT = '[Shopping List] START_EDIT';
+export const STOP_EDIT = '[Shopping List] STOP_EDIT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // Our new AddIngredient class will inherit properties of Action class. And then we add a readonly property that simply tells TypeScript that this property must never be changed.
