ccb5f677f6c759e0bcd605b131e92a56aa3da310
Adan Huerta <twinadanz@gmail.com>
2020-01-06 13:51:12 -0600

    update rxjs calls to use store actions instead

--- recipe-book-app/src/app/auth/auth.service.ts
+++ recipe-book-app/src/app/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { throwError, BehaviorSubject } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { User } from './user/user.model';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData {
   kind: string;
@@ -25,7 +28,8 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private router: Router) { }
+    private router: Router,
+    private store: Store<fromApp.AppState>) { }
 
   public signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
@@ -92,14 +96,23 @@ export class AuthService {
     );
 
     if (loadedUser.token) {
-      this.user.next(loadedUser);
+      // this.user.next(loadedUser);
+      this.store.dispatch(new AuthActions.Login(
+        {
+          email: loadedUser.email,
+          userId: loadedUser.id,
+          token: loadedUser.token,
+          expirationDate: new Date(userData._tokenExpirationDate)
+        })
+      );
       const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
       this.autoLogout(expirationDuration);
     }
   }
 
   public logout() {
-    this.user.next(null);
+    // this.user.next(null);
+    this.store.dispatch(new AuthActions.Logout());
     this.router.navigate(['/auth']);
     localStorage.removeItem('userData');
     if (this.tokenExpirationTimer) {
@@ -145,7 +158,13 @@ export class AuthService {
       token,
       expirationDate
     );
-    this.user.next(user);
+    // this.user.next(user);
+    this.store.dispatch(new AuthActions.Login({
+          email: email,
+          userId: userId,
+          token: token,
+          expirationDate: new Date(expirationDate)
+    }))
     this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user)); // We can access the browser's local storage to store data from our application.
   }
