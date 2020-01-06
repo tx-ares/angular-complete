d6e46dca71239ecab44148120cd718855bd98276
Adan Huerta <twinadanz@gmail.com>
2020-01-06 14:07:00 -0600

    update service calls to use store actions

--- recipe-book-app/src/app/auth/auth-interceptor.service.ts
+++ recipe-book-app/src/app/auth/auth-interceptor.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpParams } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { take, exhaustMap } from 'rxjs/operators';
+import { take, exhaustMap, map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private store: Store<fromApp.AppState>
+  ) { }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler) {
-    return this.authService.user.pipe(
+    // return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1), // 'take' is a rxjs operator which "takes" however many values from an obserable and then unsubscribes once that value is reached.
+      map(authState => {
+        return authState.user;
+      }),
       exhaustMap(user => { // exhaustMap is another rxjs operator which will replace our initial obserable ( the authService.user ) with another observable, which will be our http request instead.  This is a good way to get a value from one obserable and make it available to another without keeping the subscription to the first observable.
         if (!user) {
           return next.handle(req);
