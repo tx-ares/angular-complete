f4a2555d5247035997f08332bf37f5aeaa1ee45b
Adan Huerta <twinadanz@gmail.com>
2020-01-06 15:02:37 -0600

    add action prefixes

--- recipe-book-app/src/app/auth/store/auth.actions.ts
+++ recipe-book-app/src/app/auth/store/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from "@ngrx/store";
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] LOGIN';
+export const LOGOUT = '[Auth] LOGOUT';
 
 export class Login implements Action {
   readonly type = LOGIN;
