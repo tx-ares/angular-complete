5fa94becc4916f3bfca0cb92e1d34e0015fdc303
Adan Huerta <twinadanz@gmail.com>
2020-01-06 13:18:41 -0600

    add switch cases for login / logout

--- recipe-book-app/src/app/auth/store/auth.reducer.ts
+++ recipe-book-app/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user/user.model";
+import * as AuthActions from "./auth.actions";
 
 export interface State {
   user: User;
@@ -8,6 +9,21 @@ const initialState: State = {
   user: null
 }
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+
+  switch (action.type) {
+    case AuthActions.LOGIN:
+        const loggedInUser = new User(
+          action.payload.email,
+          action.payload.userId,
+          action.payload.token,
+          action.payload.expirationDate);
+    return {
+      ...state, // Always copy previous state.
+      user: loggedInUser // Set user as the new user created above.
+    }
+    case AuthActions.LOGOUT:
+    default:
+        return state;
+  }
 }
