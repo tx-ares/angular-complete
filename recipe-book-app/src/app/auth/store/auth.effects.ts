322930ad544140bf62154af61c9a0085db400c9c
Adan Huerta <twinadanz@gmail.com>
2020-02-12 15:56:24 -0600

    refactor handleAuth and handleError functions

--- recipe-book-app/src/app/auth/store/auth.effects.ts
+++ recipe-book-app/src/app/auth/store/auth.effects.ts
@@ -19,11 +19,67 @@ export interface AuthResponseData {
   registered?: boolean;
 };
 
+const handleAuthentication = (expiresIn: number, email: string, userId: string, token: string) => {
+    const expirationDate = new Date(
+      new Date().getTime() + +expiresIn * 1000  // Date().getTime() will return the current time stamp in milliseconds.  Adding a + sign infront of a variable will change it to number type if possible.
+    );
+    return new AuthActions.AuthSuccess({
+      email: email,
+      userId: userId,
+      token: token,
+      expirationDate: expirationDate
+    });
+};
+
+const handleError = (errorResponse: any) => {
+  let errorMessage = 'An unknown error occurred.';
+
+  if (!errorResponse.error || !errorResponse.error.error) {
+    return of(new AuthActions.AuthFail(errorMessage))
+  }
+  switch (errorResponse.error.error.message) {
+    case 'EMAIL_EXISTS':
+      errorMessage = 'This email exists already.';
+      break;
+    case 'EMAIL_NOT_FOUND':
+      errorMessage = 'Invalid credentials.';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMessage = 'Invalid credentials.';
+      break;
+  }
+
+  return of(new AuthActions.AuthFail(errorMessage));
+}
+
 @Injectable()
 export class AuthEffects { // Effect classes are simply used to house additional logic to be ran when a specific action is dispatched.  It can also be used to dispatch more actions.
   @Effect()
   authSignup = this.actions$.pipe(
-    ofType(AuthActions.SIGNUP_START)
+    ofType(AuthActions.SIGNUP_START),
+    switchMap((signupAction: AuthActions.SignupStart) => {
+      return this.http.post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKEY,
+        {
+          email: signupAction.payload.email,
+          password: signupAction.payload.password,
+          returnSecureToken: true
+        }
+      )
+        .pipe(
+          map( resData => {
+            return handleAuthentication(
+              +resData.expiresIn,
+              resData.email,
+              resData.localId,
+              resData.idToken
+            )
+          }),
+          catchError(errorResponse => {
+            return handleError(errorResponse);
+          })
+        );
+    })
   )
 
   @Effect()
@@ -37,40 +93,19 @@ export class AuthEffects { // Effect classes are simply used to house additional
           password: authData.payload.password,
           returnSecureToken: true
         }
-      ).pipe(
-        map(resData => {
-          const expirationDate = new Date(
-            new Date().getTime() + +resData.expiresIn * 1000  // Date().getTime() will return the current time stamp in milliseconds.  Adding a + sign infront of a variable will change it to number type if possible.
-          );
-          return new AuthActions.AuthSuccess({
-            email: resData.email,
-            userId: resData.localId,
-            token: resData.idToken,
-            expirationDate: expirationDate
-          });
+      )
+      .pipe(
+        map( resData => {
+          return handleAuthentication(
+            +resData.expiresIn,
+            resData.email,
+            resData.localId,
+            resData.idToken
+          )
         }),
         catchError(errorResponse => {
-          // Since we are catching the error from within the chain, this return value must not be an error object, so that the rest of the chain will complete.
-
-          let errorMessage = 'An unknown error occurred.';
-
-          if (!errorResponse.error || !errorResponse.error.error) {
-            return of(new AuthActions.AuthFail(errorMessage))
-          }
-          switch (errorResponse.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already.';
-              break;
-            case 'EMAIL_NOT_FOUND':
-              errorMessage = 'Invalid credentials.';
-              break;
-            case 'INVALID_PASSWORD':
-              errorMessage = 'Invalid credentials.';
-              break;
-          }
-
-          return of(new AuthActions.AuthFail(errorMessage));
-        }),
+          return handleError(errorResponse);
+        })
       );
     })
   );
