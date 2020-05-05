e7df8ddd76451fc31dc869cb431e45febb6675fe
tx-ares <twinadanz@gmail.com>
2020-05-04 21:46:04 -0500

    refactor autologin from service to store

--- recipe-book-app/src/app/app.component.ts
+++ recipe-book-app/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
 
+import * as fromApp from './store/app.reducer';
+import * as AuthActions from './auth/store/auth.actions';
+import { Store } from '@ngrx/store';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,11 +17,13 @@ export class AppComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<fromApp.AppState>
   ) { }
 
   public ngOnInit() {
-    this.authService.autoLogin();
+    // this.authService.autoLogin();
+    this.store.dispatch(new AuthActions.AutoLogin());
     this.loggingService.printLog('Hello from AppComponent ngOnInit!');
   }
 }
